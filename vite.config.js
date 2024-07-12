6e534a3e97baa38288373701e4b4a8f7925be7f4
mikasa <onlymisaky@gmail.com>
2024-07-12 17:40:21 +0800

    🐎 ci: 前端改用 React

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react-swc'
 import serverPlugin from './vite-plugins/vite-server-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), serverPlugin()],
+  plugins: [react(), serverPlugin()],
 });
